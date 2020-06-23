492dd38b8e8be2bd4a9bd8cdb9b244f3860e8986
root <root@vagrant.vm>
2020-06-23 00:07:06 +0000

    Added print code for Jenkibns testing

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World! Sourims Code ABC-123'))
+app.get('/', (req, res) => res.send('Server-1'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
